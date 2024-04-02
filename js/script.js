0652f855a7acae0f835cc29593e4b814b04a3388
Leo Chen <46356943+SSShee77y@users.noreply.github.com>
2024-04-02 14:45:45 -0400

    Added rain

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+window.addEventListener('load', function () {
+    waterManager();
+});
+
+/* Wet weather manager */
+function waterManager() {
+    const numRaindrops = 50;
+    const rainContainer = document.getElementById('storm-container');
+
+    for (let i = 0; i < numRaindrops; i++) {
+        const raindrop = document.createElement('div');
+        raindrop.classList.add('raindrop');
+        resetRaindropPosition(raindrop);
+        rainContainer.appendChild(raindrop);
+
+        randomTime = 0.1 + Math.random() * 0.4;
+        randomTimeMs = randomTime * 1000;
+        raindrop.style.animationDuration = `${randomTime}s`;
+        raindrop.style.width = `${6 - randomTime * 8}px`;
+        raindrop.style.height = `${120 - randomTime * 120}px`;
+
+        setInterval(function () {
+            resetRaindropPosition(raindrop);
+        }, randomTimeMs);
+    }
+}
+
+function resetRaindropPosition(raindrop) {
+    raindrop.style.left = `${Math.random() * 160}vw`;
+}
